cfe4335a542fc0f5f53397b11ca8871e880fc6a1
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-23 18:24:44 +0500

    Added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <div className='header__left'>
+        <a href='/' className='header__logo'>
+          armaggedon V
+        </a>
+        <span className='header__logo-descr'>
+          Сервис мониторинга и уничтожения астероидов, опасно подлетающих к
+          Земле
+        </span>
+      </div>
+      <nav className='header__nav'>
+        <NavLink
+          to='/asteroids'
+          activeClassName='header__link_active'
+          className='header__link'
+        >
+          <span>Астероиды</span>
+        </NavLink>
+        <NavLink
+          to='/destruction'
+          activeClassName='header__link_active'
+          className='header__link'
+        >
+          <span>Уничтожение</span>
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
