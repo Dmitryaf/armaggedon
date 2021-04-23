abfba0cbe5c46e796baa09115d7c6a0672ce6530
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-23 18:25:36 +0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import { Route, Redirect, BrowserRouter, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Asteroids from './pages/Asteroids/Asteroids';
+
 function App() {
-  return <div className='App'>test</div>;
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        <div className='container'>
+          <Header />
+          <Switch>
+            <Route path='/asteroids' component={Asteroids} />
+            <Route path='/destruction' />
+            <Redirect to='/asteroids' />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
