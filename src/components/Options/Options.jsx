3b877658b0142f0961e3d8b31036169684c497e8
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-24 21:06:44 +0500

    Added handlers for options

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setMeasure } from '../../redux/dataReducer';
 import './Options.scss';
 
 export default function Options() {
+  const dispatch = useDispatch();
+
+  const clickHandler = (measure) => {
+    dispatch(setMeasure(measure));
+  };
+
   return (
     <div className='options'>
-      <button type='button' className='options__btn options__btn_active'>
-        Расстояние <span className='options__btn-text'>в километрах</span>,{' '}
+      <button
+        type='button'
+        onClick={() => clickHandler('км')}
+        className='options__btn options__btn_active'
+      >
+        Расстояние <span className='options__btn-text'>в километрах</span>,
       </button>
-      <button type='button' className='options__btn'>
+      <button
+        type='button'
+        onClick={() => clickHandler('лунар')}
+        className='options__btn'
+      >
         <span className='options__btn-text'></span> в дистациях до луны
       </button>
     </div>
