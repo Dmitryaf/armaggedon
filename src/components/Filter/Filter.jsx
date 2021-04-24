517ba81f9fcb841b5eb72990b640cf24a88fdef7
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-24 21:06:09 +0500

    Added filter handler

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleIsDanger } from '../../redux/dataReducer';
 import './Filter.scss';
 
 export default function Filter() {
+  const dispatch = useDispatch();
+
+  const data = useSelector((state) => state.dataReducer.data);
+
+  const clickHandler = () => {
+    dispatch(toggleIsDanger());
+  };
   return (
-    <div className='filter'>
-      <label className='filter__label'>
+    <div className='filter' onClick={clickHandler}>
+      <div className='filter__label'>
         Показать только опасные
         <input type='checkbox' className='filter__input' />
         <span className='filter__mark'></span>
-      </label>
+      </div>
     </div>
   );
 }
