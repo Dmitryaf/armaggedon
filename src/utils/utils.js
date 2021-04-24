524ac706c6c8148de592e8ac27624e5ef2d028a8
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-24 21:07:51 +0500

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,23 +1,42 @@
-export function getCurrentDate() {
-  const date = new Date();
-  const day = date.getDay();
-  const month = date.getMonth();
-  const year = date.getFullYear();
-  return `${year}-${month}-${day}`;
+// Класс для вспомогательных фунций
+class Utils {
+  // Получение текущей даты
+  static getCurrentDate() {
+    const date = new Date();
+    const day = date.getDay();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    return `${year}-${month}-${day}`;
+  }
+
+  // Фильтрация по актуальным датам
+  static filterDate(dateArray) {
+    return dateArray.filter(
+      (item) => item.close_approach_date >= this.getCurrentDate()
+    );
+  }
+
+  // Трансформирует приходящую дату в нужный формат
+  static transformDate(transformDate) {
+    const date = new Date(transformDate);
+    const params = date.toLocaleString('ru', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+    return params.replace(/[\г.]/g, '');
+  }
+
+  // Трансформирует приходящее значение дистанции в нужный формат
+  static transformDistance(transformDistanse) {
+    return Math.floor(transformDistanse).toLocaleString('ru');
+  }
+
+  static getDistance(measureDistance, distance) {
+    return measureDistance === 'км'
+      ? `${this.transformDistance(distance.kilometers)}`
+      : `${this.transformDistance(distance.lunar)}`;
+  }
 }
 
-export function filterDate(dateArray) {
-  return dateArray.filter(
-    (item) => item.close_approach_date >= getCurrentDate()
-  );
-}
-
-export function transformDate(transformDate) {
-  const date = new Date(transformDate);
-  const params = date.toLocaleString('ru', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
-  });
-  return params.replace(/[\г.]/g, '');
-}
+export default Utils;
