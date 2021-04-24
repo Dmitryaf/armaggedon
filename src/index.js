63d70bfe3a7f2e1149705bc0f55f0ca7f003f59f
Dmitry <dmitrytbp@gmail.com>
2021-04-24 15:53:08 +0500

    Renamed file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './redux/store';
-import './scss/gobal.scss';
+import './scss/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
