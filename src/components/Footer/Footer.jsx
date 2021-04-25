b7aa868cbf073c2f286e837318adedb68ce75822
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-25 22:06:24 +0500

    Added footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <footer className='footer'>2021 © Все права и планеты защищены</footer>
+  );
+}
