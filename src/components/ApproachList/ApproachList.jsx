7f1bd600716bacf0401032732d3ebaa50a7882a2
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-28 11:14:28 +0500

    Added data from state

--- src/components/ApproachList/ApproachList.jsx
+++ src/components/ApproachList/ApproachList.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
 import ApproachItem from './ApproachItem/ApproachItem';
 import './ApproachList.scss';
 
-export default function ApproachList(props) {
-  const { measure, data } = props;
+export default function ApproachList() {
+  const cardData = useSelector((state) => state.dataReducer.currentItem)
+    .close_approach_data;
+  const measureData = useSelector((state) => state.dataReducer.measure);
+
   return (
     <div className='approach-list'>
-      {data.map((item) => {
-        return <ApproachItem key={uuidv4()} measure={measure} info={item} />;
+      {cardData.map((item) => {
+        return (
+          <ApproachItem key={uuidv4()} measure={measureData} info={item} />
+        );
       })}
     </div>
   );
