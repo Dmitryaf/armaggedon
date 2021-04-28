020357fc377cbd50ac42a64f43d54b99e9faacae
Dmitry <38415594+Dmitryaf@users.noreply.github.com>
2021-04-28 13:36:40 +0500

    Added redux-localstorage-simple

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,17 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { save, load } from 'redux-localstorage-simple';
 import dataReducer from './dataReducer';
 
 const rootReducer = combineReducers({
   dataReducer: dataReducer,
 });
 
-const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
-);
+const createStoreWithMiddleware = composeWithDevTools(
+  applyMiddleware(save(), thunk)
+)(createStore);
+
+const store = createStoreWithMiddleware(rootReducer, load());
 
 export default store;
